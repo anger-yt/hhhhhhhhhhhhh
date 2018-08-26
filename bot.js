7642501f2b187ec73122f5e0aec8d62b9acafaee
SKARHM <42718026+SKARHM@users.noreply.github.com>
2018-08-26 23:44:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,28 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '.'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
-client.login(process.env.BOT_TOKEN);
+const client = new Discord.Client({disableEveryone: true})
+const Music = require('discord.js-musicbot-addon');
+client.login(`حط توكن بوتك هنا`)
+client.on('ready', ()=> {
+    console.log('Ready!')
+})
+ Music.start(client, {
+    prefix: "!", // البرفكس هنا
+    youtubeKey: 'AIzaSyDIv8O9OwZdoTaghK3_SQmyGcQFzkXMW6E', //هنا ال yt v3 api key
+    global: false, // يكون للسيرفرات ؟ خليه false, يكون queue مشترك خليه true            
+    maxQueueSize: 50, // اقصى حجم لل queue        
+    playCmd: 'play', // امر التشغيل
+    volumeCmd: 'vol',     // امر الصوت
+    thumbnailType: 'high',
+    leaveCmd: 'stop',    // امر المغادرة
+    anyoneCanSkip: true, // تبي الكل يقدر يسوي skip? true > yep, false > nope
+    disableLoop: false, // تبي يكون فيه اعادة؟ , true > yep, false > nope
+    searchCmd: 'search', //امر البحث 
+    requesterName: true, 
+    inlineEmbeds: false,     
+    queueCmd: 'queue', // امر القائمة
+    pauseCmd: 'pause', // امر الايقاف المؤقت
+    resumeCmd: 'resume', // امر الاستمرار
+    skipCmd: 'skip', // امر السكب
+    loopCmd: 'loop', // امر الاعادة
+    enableQueueStat: true,
+  });
